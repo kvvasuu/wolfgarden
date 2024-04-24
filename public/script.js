9fd3b35f0c4e3ace9b92670aa301f42e60ce8267
kvvasu <lukaszkwas96@gmail.com>
2024-04-24 18:30:01 +0200

    ciąg dalszy

---
+++ public/script.js
@@ -0,0 +1,26 @@
+const arrow = document.getElementById("arrow-button");
+const aboutUs = document.getElementById("about-us");
+const home = document.getElementById("main");
+const content = document.getElementById("content");
+
+const jumpToHome = () => {
+  home.scrollIntoView(true);
+};
+
+arrow.addEventListener("click", jumpToHome);
+
+function scrollCheck() {
+  if (window.scrollY > 300) {
+    content.classList.add("slideUp");
+  }
+  if (window.scrollY < 400) {
+    arrow.classList.remove("scroll-nav-bottom");
+  }
+  if (window.scrollY > 800) {
+    arrow.classList.add("scroll-nav-bottom");
+  }
+  console.log(window.scrollY);
+}
+window.onscroll = function () {
+  scrollCheck();
+};
