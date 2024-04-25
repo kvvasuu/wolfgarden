1d6a1ed271b0aa3e7e7669bf20853983a878ef10
Kwasu <lukaszkwas96@gmail.com>
2024-04-25 12:42:56 +0200

    wersja alpha do kontroli

--- public/script.js
+++ public/script.js
@@ -25,3 +25,36 @@ function scrollCheck() {
 window.onscroll = function () {
   scrollCheck();
 };
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+setInterval(() => {
+  plusSlides(1);
+}, 5000);
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+document.getElementById("prev").addEventListener("click", () => plusSlides(-1));
+document.getElementById("next").addEventListener("click", () => plusSlides(1));
